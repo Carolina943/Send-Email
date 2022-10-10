9e93594ec0f2f2b5e91bc89e3027add6dbc279e6
Carolina943 <akamifox943@gmail.com>
2022-10-10 14:10:04 -0400

    modified files

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ dotenv.config()
 const express = require('express')
 const app = express()
 
+const sendEmail = require('./controllers/sendEmail')
+
+// error handler
+const notFoundMiddleware = require('./middleware/error-handler')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+//routes
+app.get('/', (req, res) =>{ 
+  res.send('<h1>Email Proyect</h1><a href="/send">send email</a>')
+})
+
+app.get('/send', sendEmail)
+
+//app.use
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+app.use(express.json())
+
 //connectDB
 const port = process.env.PORT || 3000
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,7 @@
 const {StatusCodes} = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) =>{
+    
   let customError ={
     StatusCode: err.StatusCode ||StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong try again later'
